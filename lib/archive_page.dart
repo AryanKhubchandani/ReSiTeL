28316298869505cc4544bc71fb7d43e482d7c5b5
Aryan <aryan.khubchandani@gmail.com>
2021-11-12 12:20:20 +0530

    add archive page

---
+++ lib/archive_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Archive extends StatefulWidget {
+  const Archive({Key? key}) : super(key: key);
+
+  @override
+  _ArchiveState createState() => _ArchiveState();
+}
+
+class _ArchiveState extends State<Archive> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Colors.teal,
+    );
+  }
+}
