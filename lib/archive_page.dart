95292b66bea12b47eac0bed7212353988f9c42fb
Aryan <aryan.khubchandani@gmail.com>
2021-11-13 00:50:25 +0530

    update archive section

--- lib/archive_page.dart
+++ lib/archive_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
+import 'package:share_plus/share_plus.dart';
 
 class Archive extends StatefulWidget {
   const Archive({Key? key}) : super(key: key);
@@ -11,15 +14,65 @@ class _ArchiveState extends State<Archive> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.teal,
-      body: Container(
-        padding: const EdgeInsets.all(10.0),
+      body: AnimationLimiter(
         child: ListView.builder(
-          itemBuilder: (_, index) => Text(
-            "Item $index",
-            style: const TextStyle(fontSize: 30),
-          ),
-          itemCount: 20,
+          itemCount: 10,
+          itemBuilder: (BuildContext context, int index) {
+            return AnimationConfiguration.staggeredList(
+              position: index,
+              duration: const Duration(milliseconds: 600),
+              child: SlideAnimation(
+                verticalOffset: 50.0,
+                child: FadeInAnimation(
+                  child: Slidable(
+                    key: const ValueKey(0),
+                    startActionPane: ActionPane(
+                      motion: const ScrollMotion(),
+                      dismissible: DismissiblePane(onDismissed: () {}),
+                      children: [
+                        SlidableAction(
+                          onPressed: (BuildContext context) {},
+                          backgroundColor: const Color(0xFFFE4A49),
+                          foregroundColor: Colors.white,
+                          icon: Icons.delete,
+                          label: 'Delete',
+                        ),
+                      ],
+                    ),
+                    endActionPane: ActionPane(
+                      motion: const ScrollMotion(),
+                      children: [
+                        SlidableAction(
+                          onPressed: (BuildContext context) {
+                            Share.share(
+                              "HEYY",
+                            );
+                          },
+                          backgroundColor: const Color(0xFF21B7CA),
+                          foregroundColor: Colors.white,
+                          icon: Icons.share,
+                          label: 'Share',
+                        ),
+                      ],
+                    ),
+                    child: Card(
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: <Widget>[
+                          ListTile(
+                            leading: const Icon(Icons.arrow_forward_ios),
+                            title: Text('Message $index'),
+                            subtitle: const Text(
+                                'Lorem ipsum dolor sit amet, consectetur.......'),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            );
+          },
         ),
       ),
     );
